97efbb6dbd343ae853d8127dee13c496121bcf1a
YANUAR <yanuar.0901@students.amikom.ac.id>
2019-11-26 21:31:28 +0700

    membuat input and output  pada python

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-
-print ('hello world')
+"""
+a = 5
+nama = 'yanuar'
+print ('nilai variabel a adalah',a )
+print ('nama saya adalah %s' %nama)
+print ('nama saya %s dengan umur %d' %(nama ,a))
+"""
+num = input ('masukan angka : ')
+print ('umur kamu adalah', num)
