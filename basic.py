4b50638328593b5906a50de671e6053cbfd062e8
DemonLordZeus-95 <zhccccastleclash@gmail.com>
2019-12-08 23:28:07 +0700

    penambahan materi

--- basic.py
+++ basic.py
@@ -1,4 +1,52 @@
+"""DATE TIME"""
 import datetime
 mynow = datetime.datetime.now()
 print ('tanggal hari ini', mynow)
 
+"""menggabukan variabel string dan integer"""
+number = 10
+string = 'yanuar'
+print(number,string)
+
+""" simpe type """
+
+x = 10
+y = 10
+z = 15.1
+
+hasil1 = x+x
+hasil2 = y+y
+hasil3 = z+z
+
+print (hasil1,hasil2,hasil3)
+print (type(x),type(y),type(z))
+
+"""LIST"""
+
+print (list(range(1,20,3)))
+
+temperature = [12.5 , 10, 'yanuar']
+print (temperature)
+
+""" calculating list"""
+students_grades = [2.5,5.4,7.5,8.5,9.5]
+mysum = sum(students_grades)
+print (mysum)
+length = len(students_grades)
+print (length)
+mean = mysum / length
+print(mean)
+
+"""cara mencari nilai dalam list"""
+students_grades = [10.0,10.5,11.5,10.0,12.0]
+print(students_grades.count(10.0))
+
+students = {'sabil': 7.9,'yanuar':8.0,'agung':8.5}
+print(students.values())
+print(students.keys())
+
+"""cara membuat tampilan huruf menjadi huruf kecil"""
+username = 'Python'
+print(username.lower())
+
+"""membuat Sebuah 
