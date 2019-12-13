7159a3a01c3cdd58d33467eb56c0b9a5124d6953
DemonLordZeus-95 <zhccccastleclash@gmail.com>
2019-12-13 16:43:11 +0700

    fisika

---
+++ fisika.py
@@ -0,0 +1,6 @@
+list1 = ['senin','selasa','rabu','kamis','jumat','sabtu','minggu']
+list2 = [1,2,3,4,5,6,7]
+list3 = ['hari ke1: ','hari ke2:','hari ke3:','hari ke4:','hari ke5:',
+	'hari ke6:','hari ke7']
+
+print (list1,;ist2,list3)
