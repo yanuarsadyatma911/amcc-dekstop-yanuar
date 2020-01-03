69901deae771669b294d8a6074a2388ef33ae5a7
yanuarsadyatma911 <yanuar.0901@students.amikom.ac.id>
2020-01-04 00:14:11 +0700

    for

--- bintang.py
+++ bintang.py
@@ -18,7 +18,7 @@ while bar <= x:
 print (string)
 
 
-#membuat piramid
+# #membuat piramid segitiga
 string = ""
 
 x = int(input("Masukkan angka :"))
@@ -46,101 +46,52 @@ while bar >= 0:
 	
 print (string)
 
-#
+# # BELAH KETUPAT
 
-string = ""
+# string = ""
 
-x = int(input("Masukkan angka :"))
-bar = x
-# Looping Baris
-while bar >= 0:
-	# Looping Kolom Spasi Kosong
-	kol = bar
-	while kol > 0:
-		string = string + "   "
-		kol = kol - 1
-	# Looping Kolom Bintang Sisi Kiri		
-	kiri = 1
-	while kiri < (x - (bar-1)):
-		string = string + " * "
-		kiri = kiri + 1		
-	# Looping Kolom Bintang Sisi Kanan
-	kanan = 1
-	while kanan < kiri -1:
-		string = string + " * "
-		kanan = kanan + 1	
+# x = int(input("Masukkan angka :"))
+# bar = x
+# # Looping Baris
+# while bar >= 0:
+# 	# Looping Kolom Spasi Kosong
+# 	kol = bar
+# 	while kol > 0:
+# 		string = string + "   "
+# 		kol = kol - 1
+# 	# Looping Kolom Bintang Sisi Kiri		
+# 	kiri = 1
+# 	while kiri < (x - (bar-1)):
+# 		string = string + " * "
+# 		kiri = kiri + 1		
+# 	# Looping Kolom Bintang Sisi Kanan
+# 	kanan = 1
+# 	while kanan < kiri -1:
+# 		string = string + " * "
+# 		kanan = kanan + 1	
 
-	string = string + "\n\n"
-	bar = bar - 1
+# 	string = string + "\n\n"
+# 	bar = bar - 1
 
-bar = 1	
-# Looping Baris
-while bar <= x:
-	kol = bar+1
-	# Looping Kolom Spasi Kosong
-	while kol > 1:
-		string = string + "   "
-		kol = kol - 1
-	# Looping Kolom Bintang Sisi Kiri	
-	kiri = 0
-	while kiri < (x - bar):
-		string = string + " * "
-		kiri = kiri + 1	
-	# Looping Kolom Bintang Sisi Kanan
-	kanan = kiri	
-	while kanan > 1:
-		string = string + " * "
-		kanan = kanan - 1
+# bar = 1	
+# # Looping Baris
+# while bar <= x:
+# 	kol = bar+1
+# 	# Looping Kolom Spasi Kosong
+# 	while kol > 1:
+# 		string = string + "   "
+# 		kol = kol - 1
+# 	# Looping Kolom Bintang Sisi Kiri	
+# 	kiri = 0
+# 	while kiri < (x - bar):
+# 		string = string + " * "
+# 		kiri = kiri + 1	
+# 	# Looping Kolom Bintang Sisi Kanan
+# 	kanan = kiri	
+# 	while kanan > 1:
+# 		string = string + " * "
+# 		kanan = kanan - 1
 
-	string = string + "\n\n"
-	bar = bar + 1
-print (string)
-
-#belah ketupat
-string = ""
-
-x = int(input("Masukkan angka :"))
-bar = x
-# Looping Baris
-while bar >= 0:
-	# Looping Kolom Spasi Kosong
-	kol = bar
-	while kol > 0:
-		string = string + "   "
-		kol = kol - 1
-	# Looping Kolom Bintang Sisi Kiri		
-	kiri = 1
-	while kiri < (x - (bar-1)):
-		string = string + " * "
-		kiri = kiri + 1		
-	# Looping Kolom Bintang Sisi Kanan
-	kanan = 1
-	while kanan < kiri -1:
-		string = string + " * "
-		kanan = kanan + 1	
-
-	string = string + "\n\n"
-	bar = bar - 1
-
-bar = 1	
-# Looping Baris
-while bar <= x:
-	kol = bar+1
-	# Looping Kolom Spasi Kosong
-	while kol > 1:
-		string = string + "   "
-		kol = kol - 1
-	# Looping Kolom Bintang Sisi Kiri	
-	kiri = 0
-	while kiri < (x - bar):
-		string = string + " * "
-		kiri = kiri + 1	
-	# Looping Kolom Bintang Sisi Kanan
-	kanan = kiri	
-	while kanan > 1:
-		string = string + " * "
-		kanan = kanan - 1
-
-	string = string + "\n\n"
-	bar = bar + 1
-print (string)
+# 	string = string + "\n\n"
+# 	bar = bar + 1
+# print (string)
